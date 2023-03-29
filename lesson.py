bc0dc3cfad6144a56d872e14ffb67ffadd2eec54
tethet <106799423+tthet @users.noreply.github.com>
2023-03-29 16:29:59 +0900

    メモの追加

--- lesson.py
+++ lesson.py
@@ -1,1 +1,15 @@
 print("Hello World!")
+#progateで学習した。
+# おすすめの学習サイトのまとめを見たので上から基礎を学習する
+#https://proengineer.internous.co.jp/content/columnfeature/12344
+# 
+# おすすめの書籍を調べた
+#https://www.sejuku.net/blog/3319
+#  ↓ピックアップ
+#?Python1年生　体験してわかる！会話でまなべる！プログラミングのしくみ
+#?プログラミング超初心者が初心者になるためのPython入門（１）　セットアップ・文字列・数値編
+#?独習Python
+#?Python［完全］入門
+
+#おすすめの拡張機能のまとめサイト
+#https://yurupro.cloud/767/
